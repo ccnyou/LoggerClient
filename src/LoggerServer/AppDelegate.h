cbc52d4359b554d1e58d6967fec232417e1c17fc
ervinchen <ccnyou@qq.com>
2016-10-18 12:06:46 +0800

    * 整理目录，添加 server 工程

---
+++ src/LoggerServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoggerServer
+//
+//  Created by ervinchen on 16/10/18.
+//  Copyright © 2016年 ccnyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
